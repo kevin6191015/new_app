f23d142445d3e672e187d8318a8a0d97d39797dc
Kevin Lai <kevin6191015@gmail.com>
2025-10-07 02:06:22 +0800

    change frontend home

--- frontend/src/features/home/pages/HomePage.vue
+++ frontend/src/features/home/pages/HomePage.vue
@@ -91,7 +91,7 @@ import { useRoute, useRouter } from 'vue-router'
 import { useAuthStore } from '@app/stores/useAuthStore'
 import { useCourseStore } from '@app/stores/useCourseStore'
 
-import dashboardbg from '@assets/code.png'
+import dashboardbg from '@assets/code.jpg'
 
 const pageBgVar = computed(() => ({ '--page-bg': `url(${dashboardbg})` }))
 
